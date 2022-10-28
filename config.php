ebda37708bc187d5b779bf1886282780b6ad0010
Saidu Bello <saidubello@beyonddialog.com>
2022-10-28 17:57:24 +0100

    Commit no 1

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'mega';
+$CFG->dbuser    = 'slammad';
+$CFG->dbpass    = 'Slammad42';
+$CFG->prefix    = 'cocoon_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbcollation' => 'utf8_general_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost:8888/edumy';
+$CFG->dataroot  = '/Applications/MAMP/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
